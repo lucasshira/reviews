d44ee86becc9f7ff61efbdfdf2e6d14b5b85c7b3
Lucas Gabriel <144144880+lucasshira@users.noreply.github.com>
2023-10-27 15:33:08 -0300

    Update index.js

--- index.js
+++ index.js
@@ -1,17 +1,17 @@
 const reviews = [
     {
     id: 1,
-    name: "Susan Smith",
+    name: "Chrissie Smith",
     job: "WEB DEVELOPER",
     img:
     "https://res.cloudinary.com/diqqf3eq2/image/upload/v1586883334/person-1_rfzshl.jpg",
     text:
-    "I'm baby meggings twee health goth +1. Bicycle rights tumeric chartreuse before they sold out chambray pop-up. Shaman humblebrag pickled coloring book salvia hoodie, cold-pressed four dollar."
+    "I'm baby meggings twee health goth +1. Bicycle rights tumeric chartreuse before they sold out chambray pop-up. Shaman humblebrag pickled coloring book salvia hoodie, cold-pressed four."
 },
 {
     id: 2,
-    name: "Anna Johnson",
-    job: "WEB DESIGNER",
+    name: "Mary Johnson",
+    job: "UX DESIGNER",
     img:
     "https://res.cloudinary.com/diqqf3eq2/image/upload/v1586883409/person-2_np9x5l.jpg",
     text:
@@ -19,16 +19,16 @@ const reviews = [
 },
 {
     id: 3,
-    name: "Peter Jones",
+    name: "Damon George",
     job: "INTERN",
     img:
     "https://res.cloudinary.com/diqqf3eq2/image/upload/v1586883417/person-3_ipa0mj.jpg",
     text:
-    "Sriracha literally flexitarian irony, vape marfa tumeric unicorn. Glossier tattooed 8-bit, pok fixie waistcoat offal activated charcoal slow-carb marfa hell of pabst raclette post-ironic jianbing pok swag."
+    "Sriracha literally flexitarian irony, vape marfa tumeric unicorn. Glossier tattooed 8-bit, pok fixie waistcoat offal activated charcoal slow-carb marfa hell of pabst raclette post-ironic jianbing pok."
 },
 {
     id: 4,
-    name: "Bill Anderson",
+    name: "Joseph Simon",
     job: "THE BOSS",
     img:
     "https://res.cloudinary.com/diqqf3eq2/image/upload/v1586883423/person-4_t9nxjt.jpg",
@@ -51,7 +51,6 @@ let currentItem = 0;
 window.addEventListener('DOMContentLoaded', function (){
     showPerson();
 })
-
 
 function showPerson(){
     const item = reviews[currentItem];
